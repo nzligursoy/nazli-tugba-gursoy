9fe2c03eb5079d69e63e6c704e89e51dfeddc4b9
nazli <nzligursoy61@gmail.com>
2022-04-22 11:03:45 +0300

    app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,35 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const port = process.env.PORT || 3000
 
+app.get("test_text",(req,res) => {
+    res.send("19360859037")
+})
+
+app.get("test_html",(req,res) => {
+    res.send("Nazli Tuğba Gürsoy")
+})
+
+app.get('test_json',(req,res)=>{
+    res.send([{
+        numara:19360859037   
+    },
+    {
+        ad:"Nazlı Tuğba",
+    },
+    {
+        soyad:"Gürsoy"
+    }
+    ]
+    )
+    })
+
+app.get('test_geocode',(req,res) => {
+    
+})
+
+app.get('test_weather',(req,res) => {
+    
+})
